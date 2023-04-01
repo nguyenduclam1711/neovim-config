24e004ef99fe94c48b0638bef429d8c578283e7c
Lam LUL <nguyenduclam1711@gmail.com>
2023-04-01 14:44:55 +0700

    Init lazyvim config

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,19 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end,
+    },
+    eslint = function()
+      require("lazyvim.util").on_attach(function(client)
+        if client.name == "eslint" then
+          client.server_capabilities.documentFormattingProvider = true
+        elseif client.name == "tsserver" then
+          client.server_capabilities.documentFormattingProvider = false
+        end
+      end)
+    end,
+  },
+}
