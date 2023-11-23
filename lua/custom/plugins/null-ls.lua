fb2186135242b52089ab432ddfd42a3c74717610
Lam LUL <nguyenduclam1711@gmail.com>
2023-11-23 17:46:18 +0700

    Add null-ls for golangci lint

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -8,6 +8,7 @@ return {
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.code_actions.gomodifytags,
         null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.diagnostics.golangci_lint,
       },
     })
   end,
