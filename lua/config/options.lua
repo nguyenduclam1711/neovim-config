1e416337824dbc83bc559f33ae2f481edf1e2996
Nguyen Duc Lam <lamnd2@dashsolutions.com>
2024-11-25 11:40:46 +0700

    Add config for eslint and prettier

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = true
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = true
