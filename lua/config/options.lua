f30bdf6d9220649b55b71bb807896319f6dbdb7e
Lam LUL <nguyenduclam1711@gmail.com>
2024-11-26 11:22:57 +0700

    Always wrap lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,6 @@ vim.g.lazyvim_eslint_auto_format = true
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = true
+
+-- always wrap lines
+vim.o.wrap = true
