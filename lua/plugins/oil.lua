4f42f9bc98fb20b769980e0d0cf51a7a0e765f9a
Lam LUL <nguyenduclam1711@gmail.com>
2025-01-19 22:53:53 +0700

    Update oil keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,6 +6,15 @@ return {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      ["<C-s>"] = false,
+      ["<C-h>"] = false,
+      ["<C-t>"] = false,
+      ["<C-l>"] = false,
+      ["r"] = "actions.refresh",
+      ["g."] = false,
+      ["H"] = "actions.toggle_hidden",
+    },
   },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
