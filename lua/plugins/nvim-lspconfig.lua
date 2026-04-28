88dd3b9dd6b039415765d232015cbfa1b166fb8d
Lam LUL <nguyenduclam1711@gmail.com>
2026-04-28 20:49:20 +0700

    Update lsp config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    diagnostics = {
+      virtual_text = false,
+      update_in_insert = false,
+      underline = false,
+      severity_sort = false,
+    },
     servers = {
       eslint = {},
       vtsls = {
