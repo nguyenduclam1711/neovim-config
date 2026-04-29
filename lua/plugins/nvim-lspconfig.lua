84edb475ab961988723dac13a42cf81861098799
Nguyen Duc Lam <lamnd2@dashsolutions.com>
2026-04-29 11:07:19 +0700

    Update lsp config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,12 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
-    diagnostics = {
-      virtual_text = false,
-      update_in_insert = false,
-      underline = false,
-      severity_sort = false,
-    },
     servers = {
       eslint = {},
       vtsls = {
